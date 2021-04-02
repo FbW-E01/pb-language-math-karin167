a60db66f0a13c83d90e1fd57475c3da9455a624e
karin yahud <karin167bz@gmail.com>
2021-04-02 16:56:26 +0200

    karin-task

---
+++ index.js
@@ -0,0 +1,48 @@
+//1 Minimum and maximum-Part A
+console.log("The lowest number is: " + Math.min(-1 , 0, 1, 2, 3, 4));
+
+//1 Minimum and maximum-Part B
+console.log("The highest number is: " + Math.max(-1 , 0, 1, 2, 3, 4));
+
+//2 Rounding Part A
+
+const fooOnd = 3321.32321;
+console.log(Math.ceil(fooOnd));
+
+const fooTwo = 326.76;
+console.log(Math.ceil(fooTwo));
+
+const fooTree = 76788.7;
+console.log(Math.ceil(fooTree));
+
+const foo4 = -9.78;
+console.log(Math.ceil(foo4));
+
+const foo5 = 43.342;
+console.log(Math.ceil(foo5));
+
+//2 Rounding Part B
+const mango1 = 3321.32321
+console.log(Math.floor(mango1));
+
+const mango2 = 326.76
+console.log(Math.floor(mango2));
+
+const mango3 = 76788.7
+console.log(Math.floor(mango3));
+
+const mango4 = -9.78
+console.log(Math.floor(mango4));
+
+const mango5 = 28.329
+console.log(Math.floor(mango5));
+
+//3 Dice Roll
+console.log(Math.ceil(Math.random() * 6));
+
+
+
+
+
+
+
